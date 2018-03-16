2b9843d23bb7e5fa1b587fd7d651217881e654e8
Tyler Collier <tyler.collier+github@devmounta.in>
2018-03-16 15:11:11 -0700

    Steps 6 and 7

--- src/App.js
+++ src/App.js
@@ -29,16 +29,46 @@ class App extends Component {
     })
   }
 
+  addFriend() {
+    const newFriend = {
+      pictureUrl: this.state.pictureUrl,
+      name: this.state.name,
+    };
+
+    const newFriendsList = this.state.friends.slice();
+    newFriendsList.push(newFriend);
+    this.setState(() => {
+      return {
+        friends: newFriendsList,
+        pictureUrl: '',
+        name: '',
+      }
+    });
+  }
+
   render() {
     return (
       <div>
         Picture:
-        <input onChange={event => this.updatePicture(event.target.value)} />
+        <input
+          onChange={event => this.updatePicture(event.target.value)}
+          value={this.state.pictureUrl}
+        />
 
         Name:
-        <input onChange={event => this.updateName(event.target.value)} />
+        <input
+          onChange={event => this.updateName(event.target.value)}
+          value={this.state.name}
+        />
 
-        <button>Add friend</button>
+        <button onClick={() => this.addFriend() }>Add friend</button>
+
+        <div>{
+          this.state.friends.map(friend => <div>
+            <img className="thumbnail" src={friend.pictureUrl} />
+            {friend.name}
+          </div>)
+        }</div>
 
         <div>Current state: {JSON.stringify(this.state)}</div>
       </div>
