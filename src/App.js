0aaf54b7c0c5d5436a864796e91d520bdb84aa25
Tyler Collier <tyler.collier+github@devmounta.in>
2018-03-16 14:32:29 -0700

    Step 5

--- src/App.js
+++ src/App.js
@@ -13,16 +13,34 @@ class App extends Component {
     };
   }
 
+  updatePicture(value) {
+    this.setState(() => {
+      return {
+        pictureUrl: value,
+      }
+    })
+  }
+
+  updateName(value) {
+    this.setState(() => {
+      return {
+        name: value,
+      }
+    })
+  }
+
   render() {
     return (
       <div>
         Picture:
-        <input />
+        <input onChange={event => this.updatePicture(event.target.value)} />
 
         Name:
-        <input />
+        <input onChange={event => this.updateName(event.target.value)} />
 
         <button>Add friend</button>
+
+        <div>Current state: {JSON.stringify(this.state)}</div>
       </div>
     );
   }
